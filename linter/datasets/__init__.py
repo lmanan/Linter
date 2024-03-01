5f869ec330113c0d052a0b9caf7e731e6f18f687
lmanan <manan.lalit@gmail.com>
2024-03-01 09:30:34 -0500

    Modify the __init__ file

--- linter/datasets/__init__.py
+++ linter/datasets/__init__.py
@@ -1,5 +1,5 @@
 from linter.datasets.ctc import CTCDataset
 
 
-def get_dataset(data_dir, patch_size):
-    return CTCDataset(data_dir, patch_size)
+def get_dataset(data_dir, crop_size):
+    return CTCDataset(data_dir=data_dir, crop_size=crop_size)
